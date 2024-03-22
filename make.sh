c4b9c075daf58d22bd0131b85faf1a1ec279bc34
mjr <jrgomesdev@gmail.com>
2024-03-22 02:40:31 -0300

    adicion melhorias

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
 cp .env.example .env &&
-docker compose up -d --build
-docker compose exec app composer install
-docker compose exec app php artisan key:generate
-docker compose exec app chmod 777 -Rf ./storage ./bootstrap
-docker compose exec app composer install
-docker compose exec app php artisan migrate --seed
-docker compose exec app npm install
+docker compose up -d --build &&
+docker compose exec app composer install  &&
+docker compose exec app php artisan key:generate  &&
+docker compose exec app chmod 777 -Rf ./storage ./bootstrap  &&
+docker compose exec app composer install  &&
+docker compose exec app php artisan migrate --seed  &&
+docker compose exec app npm install  &&
 docker compose exec app npm run build
